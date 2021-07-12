c02d2f30bb33e89766aefcd80ba0e92e1aee5d6e
riyamishra2730 <69318212+riyamishra2730@users.noreply.github.com>
2021-07-12 12:47:08 +0530

    Add files via upload

---
+++ Chatbot.java
@@ -0,0 +1,10 @@
+package chatbot;
+
+public class Chatbot {
+
+	public static void main(String[] args) {
+		ChatBotMain obj=new ChatBotMain();
+		obj.setTitle("ChatBot(Team-Edubridge)");
+		obj.setVisible(true);
+	}
+}
